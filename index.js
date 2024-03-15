3b811c5fade7dc66ad545835348db4602485b6ea
Vans <53773587+iamfernweh@users.noreply.github.com>
2024-03-15 12:49:07 -0400

    app made

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const path = require('path');
 const { PrismaClient } = require('@prisma/client');
 const client = new PrismaClient();
@@ -9,14 +10,74 @@ app.get('/', (req, res) =>
 );
 app.use('/assets', express.static(path.join(__dirname, '/client/dist/assets')));
 
-app.get('/api/notes', (req, res, next) => {
-  res.send([{ txt: 'hi' }]);
+app.get('/api/notes', async (req, res, next) => {
+  try {
+    res.send(await client.note.findMany());
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.put('/api/notes/:id', async (req, res, next) => {
+  try {
+    const note = await client.note.update({
+      where: {
+        id: req.params.id * 1,
+      },
+      data: {
+        isArchived: req.body.isArchived,
+      },
+    });
+    res.send(note);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete('/api/notes/:id', async (req, res, next) => {
+  try {
+    await client.note.delete({
+      where: {
+        id: req.params.id * 1,
+      },
+    });
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post('/api/notes/', async (req, res, next) => {
+  try {
+    const note = await client.note.create({
+      data: req.body,
+    });
+    res.status(201).send(note);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).send({ error: 'page not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message || err });
 });
 
 const init = async () => {
+  await client.note.deleteMany({});
   const foo = await client.note.create({
     data: {
       txt: 'foo',
+    },
+  });
+  const bar = await client.note.create({
+    data: {
+      txt: 'bar',
+      isArchived: true,
     },
   });
   const notes = await client.note.findMany();
